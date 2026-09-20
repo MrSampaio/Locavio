7784e98f62d890c9b66f43f56b5614d6d01d0003
Julio Sampaio <jusampa2@gmail.com>
2026-09-20 20:11:03 -0300

    refact: adjusts fetchUserData func to receive context parameter and implements.onAppear into PropertiesView

--- locavio/ViewModels/PropertiesViewModel.swift
+++ locavio/ViewModels/PropertiesViewModel.swift
@@ -1,41 +1,45 @@
 //
-//  HomeViewModel.swift
+//  PropertiesViewModel.swift
 //  locavio
 //
 //  Created by Julio Sampaio on 20/09/26.
 //
 
 import SwiftUI
+import SwiftData
 
 @Observable
-final class PropertiesViewModel{
+final class PropertiesViewModel {
     
-    private let propertiesCoordinator = PropertiesCoordinator()
+    var userName: String = "Carregando..."
+    var userEmail: String = "Carregando..."
     
-    var userName: String = ""
-    var userEmail: String = ""
-    
-    init() {
-        fetchUserData()
-    }
-    
-    // função para puxar os dados do usuário
-    func fetchUserData(){
-        if let savedName = KeychainHelper.shared.readString(for: "appleUserFullName"), !savedName.isEmpty{
-            self.userName = savedName
-        } else{
-            self.userName = "There is no username."
+    // função de fetch para pegar os dados do usuário pelo keychain
+    // recebe o contexto como parâmetro, já que ele só pode ser passado em structs
+    func fetchUserData(context: ModelContext) {
+        
+        // pega o id do usuário cadastrado no keychain
+        guard let userID = KeychainHelper.shared.readString(for: "appleUserID") else {
+            self.userName = "No user found"
+            return
         }
         
-        if let savedEmail = KeychainHelper.shared.readString(for: "appleUserEmail"), !savedEmail.isEmpty{
-            self.userEmail = savedEmail
-        } else{
-            self.userEmail = "There is no user email."
+        // procura no SwiftData o perfil que tem esse id
+        let descriptor = FetchDescriptor<UserProfile>(predicate: #Predicate { $0.appleUserID == userID })
+        
+        do {
+            let existingUsers = try context.fetch(descriptor)
+            
+            if let userProfile = existingUsers.first {
+                // caso tenha achado o perfil no banco, atualiza as variáveis
+                self.userName = userProfile.fullName ?? "Usuário"
+                self.userEmail = userProfile.email ?? "Sem email cadastrado"
+            } else {
+                // caso não tenha encontrado ou caso o icloud demore pra responder
+                self.userName = "No user found"
+            }
+        } catch {
+            print("Error when trying to fetch user from SwiftData: \(error)")
         }
     }
-    
-    func addNewPropertyTapped() {
-        propertiesCoordinator.pushToNewProperty()
-    }
 }
-
