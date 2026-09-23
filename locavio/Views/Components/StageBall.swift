a5350bc704200d6462aff656ce8b47d6780ccfef
Guilherme Alves de Souza <guimourast@gmail.com>
2026-09-23 17:53:44 -0300

    ui: implements stages ball component

---
+++ locavio/Views/Components/StageBall.swift
@@ -0,0 +1,34 @@
+//
+//  StageBall.swift
+//  locavio
+//
+//  Created by Guilherme Alves de Souza on 23/09/26.
+//
+
+import SwiftUI
+
+struct StageBall: View {
+
+    let currentPage: Int
+    let allPages: Int
+    
+    var body: some View {
+        HStack(spacing: 7) {
+            
+            ForEach(0..<allPages, id: \.self) { index in
+                
+                Circle()
+                    .fill(
+                        index == currentPage - 1
+                        ? (Color.primary)
+                        : Color.gray.opacity(0.5)
+                    )
+                    .frame(width: 8, height: 8)
+            }
+        }
+    }
+}
+
+#Preview {
+    StageBall(currentPage: 1, allPages: 3)
+}
